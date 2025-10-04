7f35bdb780ec4a085661eed1f6af9e6079bead69
sasayejune <yejun0626@gmail.com>
2025-10-04 23:30:30 +0900

    fix2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import Login from './components/Login';
+
+function Root() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const auth = localStorage.getItem('is_authenticated') === 'true';
+    setIsAuthenticated(auth);
+  }, []);
+
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div>
+          <h1>Welcome, Admin!</h1>
+          <button
+            onClick={() => {
+              localStorage.removeItem('is_authenticated');
+              setIsAuthenticated(false);
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <Login setIsAuthenticated={setIsAuthenticated} />
+      )}
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
